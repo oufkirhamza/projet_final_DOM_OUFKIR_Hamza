20f458624dee78245c9c00e73ddc0435b8d614de
oufkirhamza <oufkirhamza08@gmail.com>
2024-01-18 23:03:53 +0100

    modal navBar

--- public/js/app.js
+++ public/js/app.js
@@ -26,3 +26,19 @@ foodChoices[3].addEventListener('click',()=>{
     eachFoodMenu[2].classList.add("d-none")
     eachFoodMenu[0].classList.add("d-none")
 })
+
+let modalBtn = document.querySelector('.modalBtn').children
+let loginModal = document.querySelector('.LogInModal')
+let signUpModal = document.querySelector('.signUpModal')
+let modalFooterBtn = document.querySelector('.modal-footer').children
+
+modalBtn[0].addEventListener('click',()=>{
+    loginModal.classList.add("d-none")
+    signUpModal.classList.remove("d-none")
+    modalFooterBtn[1].innerHTML='Sign up'
+})
+modalBtn[1].addEventListener('click',()=>{
+    loginModal.classList.remove("d-none")
+    signUpModal.classList.add("d-none")
+    modalFooterBtn[1].innerHTML='Log in'
+})
