4f4499288ca2081ca809787c45c230e503cb1638
oufkirhamza <oufkirhamza08@gmail.com>
2024-01-19 15:13:48 +0100

    modal video

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,25 @@ let Choices = document.querySelector(".foodChoices")
 let foodMenu = document.querySelector(".foodMenu")
 let eachFoodMenu = foodMenu.children
 let foodChoices = Choices.children
-foodChoices[0].addEventListener('click',()=>{
+foodChoices[0].addEventListener('click', () => {
     eachFoodMenu[0].classList.remove("d-none")
     eachFoodMenu[1].classList.add("d-none")
     eachFoodMenu[2].classList.add("d-none")
     eachFoodMenu[3].classList.add("d-none")
 })
-foodChoices[1].addEventListener('click',()=>{
+foodChoices[1].addEventListener('click', () => {
     eachFoodMenu[1].classList.remove("d-none")
     eachFoodMenu[0].classList.add("d-none")
     eachFoodMenu[2].classList.add("d-none")
     eachFoodMenu[3].classList.add("d-none")
 })
-foodChoices[2].addEventListener('click',()=>{
+foodChoices[2].addEventListener('click', () => {
     eachFoodMenu[2].classList.remove("d-none")
     eachFoodMenu[1].classList.add("d-none")
     eachFoodMenu[0].classList.add("d-none")
     eachFoodMenu[3].classList.add("d-none")
 })
-foodChoices[3].addEventListener('click',()=>{
+foodChoices[3].addEventListener('click', () => {
     eachFoodMenu[3].classList.remove("d-none")
     eachFoodMenu[1].classList.add("d-none")
     eachFoodMenu[2].classList.add("d-none")
@@ -32,26 +32,26 @@ let loginModal = document.querySelector('.LogInModal')
 let signUpModal = document.querySelector('.signUpModal')
 let modalFooterBtn = document.querySelector('.modal-footer').children
 
-modalBtn[0].addEventListener('click',()=>{
+modalBtn[0].addEventListener('click', () => {
     loginModal.classList.add("d-none")
     signUpModal.classList.remove("d-none")
-    modalFooterBtn[1].innerHTML='Sign up'
+    modalFooterBtn[1].innerHTML = 'Sign up'
 })
-modalBtn[1].addEventListener('click',()=>{
+modalBtn[1].addEventListener('click', () => {
     loginModal.classList.remove("d-none")
     signUpModal.classList.add("d-none")
-    modalFooterBtn[1].innerHTML='Log in'
-    
+    modalFooterBtn[1].innerHTML = 'Log in'
+
 })
 let carousel = document.querySelector('.carousell')
 let arrowBtn = document.querySelectorAll('.eventCarousel i')
 let imgCardWidth = document.querySelector('.eventImg').offsetWidth;
 arrowBtn.forEach(btn => {
-    btn.addEventListener('click',()=>{
+    btn.addEventListener('click', () => {
         if (btn.id === "left") {
-            carousel.scrollLeft -= 510
-        }else{
-            carousel.scrollLeft += 510
+            carousel.scrollLeft -= imgCardWidth
+        } else {
+            carousel.scrollLeft += imgCardWidth
         }
     })
 });
@@ -61,13 +61,37 @@ let arrowBtnGAllery = document.querySelectorAll('.galleryCarousel i');
 let galleryCardWidth = document.querySelector('.GalleryImg img').offsetWidth;
 
 arrowBtnGAllery.forEach(btn => {
-    btn.addEventListener('click',()=>{
+    btn.addEventListener('click', () => {
         if (btn.id === "left") {
             galleryCarousel.scrollLeft -= 310
-        }else{
+        } else {
             galleryCarousel.scrollLeft += 310
         }
     })
 });
+let myBody = document.body
+let videoDiv = document.querySelector('.videoDiv')
+let videoDiv1 = document.querySelector('.videoDiv1')
+let heroBtn = document.querySelector('.heroBtn')
+let xDivVideo = document.querySelector('.videoDiv i')
+let xDivVideo1 = document.querySelector('.videoDiv1 i')
+heroBtn.addEventListener('click', () => {
+    videoDiv.classList.remove("d-none")
+    myBody.style.overflow = "hidden"
+    xDivVideo.addEventListener("click", () => {
+        videoDiv.classList.add("d-none")
+        myBody.style.overflow = "auto"
+    })
+})
 
+let vidBtn = document.querySelector('.button')
+vidBtn.addEventListener('click',()=>{
+    videoDiv1.classList.remove("d-none")
+    myBody.style.overflow = "hidden"
+    xDivVideo1.addEventListener("click", () => {
+        videoDiv1.classList.add("d-none")
+        myBody.style.overflow = "auto"
+    })
+
+})
 
